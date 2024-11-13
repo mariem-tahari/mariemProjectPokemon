3441664f03bec8db513f67aa67afd914f5975cfc
Thibaut <thibaut.wasilewski@icloud.com>
2024-11-13 11:16:18 +0100

    allo

--- TP/ReactTP4/react/src/App.jsx
+++ TP/ReactTP4/react/src/App.jsx
@@ -1,9 +1,9 @@
-import {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import './App.css'
 
 function App() {
-    const [livres, setLivre] = useState("")
+    const [livres, setLivre] = useState([]); // Set initial state as an empty array
 
     useEffect(() => {
         fetch("/bibliotheque")
@@ -15,17 +15,68 @@ function App() {
         console.log(livres)
     }, [livres]);
 
+
+    function handleSubmit() {
+        const titre = document.querySelector('input[name="titre"]').value;
+        const auteur = document.querySelector('input[name="auteur"]').value;
+        const nbExemplaires = document.querySelector('input[name="nbExemplaires"]').value;
+
+        addLivre(titre, auteur, nbExemplaires);
+    }
+
     return (
         <>
-            {livres.map((livre, key) => (
-                <div key={key}>
-                    <p>{livre.titre}</p>
-                    <p>{livre.auteur}</p>
-                    <p>{livre.nbExemplaires}</p>
-                </div>
-            ))}
+            <div>
+                <h1>Bibliothèque</h1>
+                <ul>
+                    {livres.map(livre => (
+                        <li key={livre.id}>
+                            <h2>{livre.titre}</h2>
+                            <p>{livre.auteur}</p>
+                            <p>{livre.nbExemplaires}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h1>Ajouter un livre</h1>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Titre:
+                        <input type="text" name="titre" />
+                    </label>
+                    <label>
+                        Auteur:
+                        <input type="text" name="auteur" />
+                    </label>
+                    <label>
+                        Nombre d&#39;exemplaires:
+                        <input type="number" name="nbExemplaires" />
+                    </label>
+                    <button type="submit">Ajouter</button>
+                </form>
+            </div>
         </>
-    )
+    );
 }
+
+
+function addLivre(titre, auteur, nbExemplaires) {
+    fetch("/bibliotheque", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            titre: titre,
+            auteur: auteur,
+            nbExemplaires: nbExemplaires
+        })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+}
+
 
 export default App
